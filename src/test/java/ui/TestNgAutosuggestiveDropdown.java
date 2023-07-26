ee5948186c8d5725112cb2ec07d4d17a6edccc9c
priyanka <priyankamohapatra2110@gmail.com>
2023-07-26 20:06:16 +0530

    new testng1

---
+++ src/test/java/ui/TestNgAutosuggestiveDropdown.java
@@ -0,0 +1,44 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class TestNgAutosuggestiveDropdown {
+	
+	
+	@Test(priority=1)
+	public void fromDropdown()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\priya\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();  
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		driver.manage().window().maximize();
+		WebElement From = driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']"));
+		From.click();
+		From.sendKeys("sa");
+		From.sendKeys(Keys.ARROW_DOWN);
+		From.sendKeys(Keys.ENTER);
+		
+		
+	}
+	@Test()
+	public void DestinationDropdown()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\priya\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();  
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.manage().window().maximize();
+		WebElement Destination = driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_destinationStation1_CTXT']"));
+		Destination.click();
+		Destination.sendKeys("BLR");
+		Destination.sendKeys(Keys.ARROW_DOWN);
+		Destination.sendKeys(Keys.ENTER);
+	}
+
+}
